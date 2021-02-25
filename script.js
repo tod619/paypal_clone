234913b4d1cb2e498924c86068ebb3a5cce70537
tod619 <trevorodwyer619@hotmail.com>
2021-02-25 16:34:19 +0000

    display menu on click in script.js

--- script.js
+++ script.js
@@ -24,26 +24,52 @@ document.querySelectorAll(".signup").forEach(signup => {
 })
 // End: Page Selection
 
-// Start: Navigation dropdown menu
-const dropdownItems = document.querySelectorAll(".dropdown-hover");
+// Navigation
+const dropdownItems = document.querySelectorAll('.dropdown-hover')
 
-dropdownItems.forEach(dropdownItem => {
-        dropdownItem.addEventListener("mouseover", () => {
-                dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible';
-                document.querySelector(".navbar-wrapper").style.background = "linear-gradient(to right, #066399, #2f8fdf, #066399)"
+if(window.innerWidth < 1000) {
+        const menuIcon = document.querySelector('.menu')
+        const navbar = document.querySelector('.navbar')
+
+        menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('change')
+
+        if(!navbar.classList.contains('change')) {
+                document.querySelectorAll('.nav-dropdown').forEach(dropdown => {
+                dropdown.style.left = '-20rem'
+                })
+                }
         })
 
-        dropdownItem.addEventListener("mouseout", () => {
-                dropdownItem.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden';
-                document.querySelector(".navbar-wrapper").style.background = "none"
+        document.querySelectorAll('.show-dropdown').forEach(link => {
+                link.addEventListener('click', () => {
+                link.nextElementSibling.style.left = '0'
+                })
         })
+
+        document.querySelectorAll('.dropdown-heading-link').forEach(headingLink => {
+                headingLink.addEventListener('click', () => {
+                headingLink.parentElement.parentElement.style.left = '-20rem'
+                })
+        })
+} else {
+        dropdownItems.forEach(dropdownItem => {
+        dropdownItem.addEventListener('mouseover', () => {
+                dropdownItem.lastElementChild.style.cssText = 'opacity: 1; visibility: visible'
+                document.querySelector('.navbar-wrapper').style.background = 'linear-gradient(to right, #066399, #2f8fdf, #066399)'
+                dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(180deg)'
+                })
+        dropdownItem.addEventListener('mouseout', () => {
+                dropdownItem.lastElementChild.style.cssText = 'opacity: 0; visibility: hidden'
+                document.querySelector('.navbar-wrapper').style.background = 'none'
+                dropdownItem.firstElementChild.firstElementChild.style.transform = 'rotate(0)'
+                })
+        })
+}
+
+window.addEventListener('resize', () => {
+        window.location.reload()
 })
 
-const menuIcon = document.querySelector(".menu");
-const navbar = document.querySelector(".navbar");
-
-menuIcon.addEventListener("click", () => {
-        navbar.classList.toggle("change");
-})
-// End: Navigation dropdown menu
+// End of Navigation
 
